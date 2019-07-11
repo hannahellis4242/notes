1a8bb15be411c28817a3aa5f56ce7d4df86baffa
hannah <hannah.ellis@pulsic.com>
2019-07-11 23:45:41 +0100

    adding a build script (which doesn't seem to work) and updating the pdfs

---
+++ build.sh
@@ -0,0 +1,8 @@
+cd $working_dir/Ellipse
+pdflatex Ellipse.tex
+pdflatex Ellipse.tex
+
+cd $working_dir/ComplexFourierSeries
+pdflatex main.tex
+pdflatex main.tex
+mv main.pdf ComplexFourierSeries.pdf
